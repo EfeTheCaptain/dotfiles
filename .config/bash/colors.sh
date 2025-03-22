7b85b55cbc66ef6b934db5506f687c18781bd29b
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-22 19:27:19 +0300

    Create colors.sh

---
+++ .config/bash/colors.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Sunny Evergreen Forest with Sky Palette
+
+# Sky Colors
+bright_sky='\[\e[38;5;117m\]'  # Light Blue (Sunny Sky)
+mid_sky='\[\e[38;5;39m\]'    # Medium Blue (Day Sky)
+cloud_white='\[\e[38;5;231m\]' # White (Clouds)
+
+# Forest Colors
+sunlit_leaves='\[\e[38;5;118m\]' # Light Green (Sunlit Leaves)
+evergreen_dark='\[\e[38;5;22m\]' # Dark Green (Evergreen Needles)
+mossy_green='\[\e[38;5;28m\]'    # Moss Green
+bark_brown='\[\e[38;5;94m\]'    # Bark Brown
+soil_brown='\[\e[38;5;52m\]'    # Soil Brown
+sun_yellow='\[\e[38;5;226m\]'   # Bright Yellow (Sun)
+forest_shadow='\[\e[38;5;16m\]' # Dark Grey (Shadows)
+
+# Reset color
+nc='\[\e[0m\]'
