802b01ec18cc199bf073d43b9c4f24593ba6b370
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-22 19:52:55 +0300

    Update and rename prompt.sh to colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Define Sunny Evergreen Forest with Sky Palette
+bright_sky='\[\e[38;5;117m\]'  # Light Blue (Sunny Sky)
+mid_sky='\[\e[38;5;39m\]'    # Medium Blue (Day Sky)
+cloud_white='\[\e[38;5;231m\]' # White (Clouds)
+sunlit_leaves='\[\e[38;5;118m\]' # Light Green (Sunlit Leaves)
+evergreen_dark='\[\e[38;5;22m\]' # Dark Green (Evergreen Needles)
+mossy_green='\[\e[38;5;28m\]'    # Moss Green
+bark_brown='\[\e[38;5;94m\]'    # Bark Brown
+soil_brown='\[\e[38;5;52m\]'    # Soil Brown
+sun_yellow='\[\e[38;5;226m\]'   # Bright Yellow (Sun)
+forest_shadow='\[\e[38;5;16m\]' # Dark Grey (Shadows)
+nc='\[\e[0m\]'
+
 # Color detection logic
 case "$TERM" in
     xterm-color) color_prompt=yes;;
@@ -12,9 +25,6 @@ if [ -n "$force_color_prompt" ]; then
         color_prompt=
     fi
 fi
-
-# Source color definitions
-source ~/.config/bash/colors.sh
 
 if [ "$color_prompt" = yes ]; then
     PS1='${debian_chroot:+($debian_chroot)}'$sunlit_leaves'\u'@$evergreen_dark'\h'$nc':\[\e[1;37m\]\w'$nc'\$ '
@@ -39,3 +49,6 @@ if [ "$UID" = 0 ]; then
 else
     PS1="$sunlit_leaves\u$nc@$evergreen_dark\H$nc:$cloud_white\w$nc\\n$sunlit_leaves\$ $nc"
 fi
+
+# Force color interpretation
+PS1="$(eval echo -e \"$PS1\")"
