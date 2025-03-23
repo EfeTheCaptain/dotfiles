6c007cf73dc9d17f8eaf94136bf2b4ee7d2685c0
EfeTheCaptain <m.tahaefe@hotmail.com>
2025-03-23 11:08:50 +0300

    .

--- .config/system/network-startup.sh
+++ .config/system/network-startup.sh
@@ -5,18 +5,18 @@ WLAN_INTERFACE="wlan0"
 WPA_CONFIG="/etc/wpa_supplicant/wpa_supplicant.conf"
 
 # Check if network is already configured
-if ip a show "$WLAN_INTERFACE" | grep -q "inet "; then
+if sudo /usr/sbin/ip a show "$WLAN_INTERFACE" | grep -q "inet "; then
   echo "Network already configured. Exiting."
   exit 0
 fi
 
 # Bring wlan0 interface up
-/usr/sbin/ip link set "$WLAN_INTERFACE" up
+sudo /usr/sbin/ip link set "$WLAN_INTERFACE" up
 
 # Start wpa_supplicant in the background
-/usr/sbin/wpa_supplicant -B -i "$WLAN_INTERFACE" -c "$WPA_CONFIG"
+sudo /usr/sbin/wpa_supplicant -B -i "$WLAN_INTERFACE" -c "$WPA_CONFIG"
 
 # Obtain IP address via DHCP
-/usr/sbin/dhclient "$WLAN_INTERFACE"
+sudo /usr/sbin/dhclient "$WLAN_INTERFACE"
 
 # Exit script
