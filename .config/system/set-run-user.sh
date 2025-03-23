b926322acf63009e38878f825a7a04b7f05acab2
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-23 10:48:44 +0300

    Update set-run-user.sh

--- .config/system/set-run-user.sh
+++ .config/system/set-run-user.sh
@@ -7,14 +7,14 @@ RUNTIME_DIR="/run/user/$USER_ID"
 # Check if the runtime directory exists
 if [ -d "$RUNTIME_DIR" ]; then
     # Check if it's owned by root
-    if [ "$(stat -c %U "$RUNTIME_DIR")" = "root" ]; then
+    if [ "$(sudo stat -c %U "$RUNTIME_DIR")" = "root" ]; then
         # Change ownership and permissions
-        chmod 0700 "$RUNTIME_DIR"
+        sudo chmod 0700 "$RUNTIME_DIR"
         if [ $? -ne 0 ]; then
             echo "Error setting permissions for: $RUNTIME_DIR"
             exit 1
         fi
-        chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR"
+        sudo chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR"
         if [ $? -ne 0 ]; then
             echo "Error setting ownership for: $RUNTIME_DIR"
             exit 1
@@ -22,18 +22,18 @@ if [ -d "$RUNTIME_DIR" ]; then
     fi
 else
     # Create the runtime directory
-    mkdir -p "$RUNTIME_DIR"
+    sudo mkdir -p "$RUNTIME_DIR"
     if [ $? -ne 0 ]; then
         echo "Error creating runtime directory: $RUNTIME_DIR"
         exit 1
     fi
     # Set ownership and permissions
-    chmod 0700 "$RUNTIME_DIR"
+    sudo chmod 0700 "$RUNTIME_DIR"
     if [ $? -ne 0 ]; then
         echo "Error setting permissions for: $RUNTIME_DIR"
         exit 1
     fi
-    chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR"
+    sudo chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR"
     if [ $? -ne 0 ]; then
         echo "Error setting ownership for: $RUNTIME_DIR"
         exit 1
