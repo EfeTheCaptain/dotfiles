899dc035da7592da941b5ab525b48f3812fb9ccf
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-23 05:44:09 +0300

    Create set-run-user.sh

---
+++ .config/system/set-run-user.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+USER_ID=1000
+GROUP_ID=1000
+RUNTIME_DIR="/run/user/$USER_ID"
+
+# Check if the runtime directory exists
+if [ -d "$RUNTIME_DIR" ]; then
+    # Check if it's owned by root
+    if [ "$(stat -c %U "$RUNTIME_DIR")" = "root" ]; then
+        # Change ownership and permissions
+        chmod 0700 "$RUNTIME_DIR"
+        if [ $? -ne 0 ]; then
+            echo "Error setting permissions for: $RUNTIME_DIR"
+            exit 1
+        fi
+        chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR"
+        if [ $? -ne 0 ]; then
+            echo "Error setting ownership for: $RUNTIME_DIR"
+            exit 1
+        fi
+    fi
+else
+    # Create the runtime directory
+    mkdir -p "$RUNTIME_DIR"
+    if [ $? -ne 0 ]; then
+        echo "Error creating runtime directory: $RUNTIME_DIR"
+        exit 1
+    fi
+    # Set ownership and permissions
+    chmod 0700 "$RUNTIME_DIR"
+    if [ $? -ne 0 ]; then
+        echo "Error setting permissions for: $RUNTIME_DIR"
+        exit 1
+    fi
+    chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR"
+    if [ $? -ne 0 ]; then
+        echo "Error setting ownership for: $RUNTIME_DIR"
+        exit 1
+    fi
+fi
+
+# Keep the service running (required for runit)
+while true; do
+    sleep 3600
+done
