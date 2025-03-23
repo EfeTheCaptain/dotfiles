f9105db65f4832d7dc52e0d3ee7b0d2cbeacbce3
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-23 03:30:13 +0300

    Update and rename xdg-runtime-dir.sh to xdg-runtime-dir

--- .config/system/xdg-runtime-dir.sh
+++
@@ -1,7 +0,0 @@
-#runit service at /etc/runit/runsvdir/default/create-run-user/run
-if [ -z "$XDG_RUNTIME_DIR" ]; then
-    export XDG_RUNTIME_DIR=/run/user/$(id -u)        # No sudo needed for export
-    sudo mkdir -p "$XDG_RUNTIME_DIR"                   # Use sudo for mkdir
-    sudo chmod 0700 "$XDG_RUNTIME_DIR"                 # Use sudo for chmod
-    sudo chown $(id -u):$(id -g) "$XDG_RUNTIME_DIR"    # Use sudo for chown
-fi
