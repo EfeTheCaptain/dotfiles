e9e36be6285539e1fadf5f0871337a2784102fc0
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-24 15:59:41 +0300

    Update colored_prompt.sh
    pywal colors

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# Define Sunny Evergreen Forest with Sky Palette
-bright_sky='\[\e[38;5;117m\]'  # Light Blue (Sunny Sky)
-mid_sky='\[\e[38;5;39m\]'    # Medium Blue (Day Sky)
-cloud_white='\[\e[38;5;231m\]' # White (Clouds)
-sunlit_leaves='\[\e[38;5;118m\]' # Light Green (Sunlit Leaves)
-evergreen_dark='\[\e[38;5;22m\]' # Dark Green (Evergreen Needles)
-mossy_green='\[\e[38;5;28m\]'    # Moss Green
-bark_brown='\[\e[38;5;94m\]'    # Bark Brown
-soil_brown='\[\e[38;5;52m\]'    # Soil Brown
-sun_yellow='\[\e[38;5;226m\]'   # Bright Yellow (Sun)
-forest_shadow='\[\e[38;5;16m\]' # Dark Grey (Shadows)
-nc='\[\e[0m\]'
+# Load Pywal colors if available
+if [[ -f ~/.cache/wal/colors.bash ]]; then
+    source ~/.cache/wal/colors.bash
+else
+    # Fallback to your original colors if Pywal isn't available
+    bright_sky='\[\e[38;5;117m\]'  # Light Blue (Sunny Sky)
+    mid_sky='\[\e[38;5;39m\]'     # Medium Blue (Day Sky)
+    cloud_white='\[\e[38;5;231m\]' # White (Clouds)
+    sunlit_leaves='\[\e[38;5;118m\]' # Light Green (Sunlit Leaves)
+    evergreen_dark='\[\e[38;5;22m\]' # Dark Green (Evergreen Needles)
+    mossy_green='\[\e[38;5;28m\]'     # Moss Green
+    bark_brown='\[\e[38;5;94m\]'     # Bark Brown
+    soil_brown='\[\e[38;5;52m\]'     # Soil Brown
+    sun_yellow='\[\e[38;5;226m\]'    # Bright Yellow (Sun)
+    forest_shadow='\[\e[38;5;16m\]' # Dark Grey (Shadows)
+    nc='\[\e[0m\]'
+fi
 
-# Color detection logic
+# Color detection logic (remains the same)
 case "$TERM" in
     xterm-color) color_prompt=yes;;
 esac
@@ -26,16 +31,16 @@ if [ -n "$force_color_prompt" ]; then
     fi
 fi
 
-# Root/user prompt
+# Root/user prompt (use Pywal colors or fallbacks)
 if [ "$UID" = 0 ]; then
-    PS1="\n\[\e[1;32m\]╭━━━[\[$bright_sky\]\u@\h\[\e[1;32m\]]━━━[\[$cloud_white\]\w\[\e[1;32m\]]\n\[\e[1;32m\]╰━━▶  :\[\e[0m\] "
+    PS1="\n\[\e[1;32m\]╭━━━[\[${color1:-$bright_sky}\]\u@\h\[\e[1;32m\]]━━━[\[${color15:-$cloud_white}\]\w\[\e[1;32m\]]\n\[\e[1;32m\]╰━━▶  :\[\e[0m\] "
 else
-    PS1="\n\[\e[1;32m\]╭━━━[\[$sunlit_leaves\]\u@\h\[\e[1;32m\]]━━━[\[$mid_sky\]\w\[\e[1;32m\]]\n\[\e[1;32m\]╰━━▶  \[\e[0m\] "
+    PS1="\n\[\e[1;32m\]╭━━━[\[${color2:-$sunlit_leaves}\]\u@\h\[\e[1;32m\]]━━━[\[${color4:-$mid_sky}\]\w\[\e[1;32m\]]\n\[\e[1;32m\]╰━━▶  \[\e[0m\] "
 fi
 
 unset color_prompt force_color_prompt
 
-# Xterm title setting
+# Xterm title setting (remains the same)
 case "$TERM" in
     xterm*|rxvt*)
         PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
@@ -44,5 +49,5 @@ case "$TERM" in
         ;;
 esac
 
-# Force color interpretation
+# Force color interpretation (remains the same)
 PS1="$(eval echo -e \"$PS1\")"
