fd28a88ec56e6c381e95c3ac82c7d09d1cd610d1
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-25 21:43:28 +0300

    Update set-run-user.sh
    silent error point out

--- .config/system/set-run-user.sh
+++ .config/system/set-run-user.sh
@@ -5,36 +5,37 @@ GROUP_ID=1000
 RUNTIME_DIR="/run/user/$USER_ID"
 
 sleep 3
-# Check if the runtime directory exists
-if [ -d "$RUNTIME_DIR" ]; then
-    # Check if it's owned by root
-    if [ "$(sudo stat -c %U "$RUNTIME_DIR")" = "root" ]; then
-        # Change ownership and permissions
-        sudo chmod 0700 "$RUNTIME_DIR"
+
+# Check if the runtime directory exists silently
+if [ -d "$RUNTIME_DIR" ] 2>/dev/null; then
+    # Check if it's owned by root silently
+    if [ "$(stat -c %U "$RUNTIME_DIR" 2>/dev/null)" = "root" ]; then
+        # Change ownership and permissions silently
+        sudo chmod 0700 "$RUNTIME_DIR" 2>/dev/null
         if [ $? -ne 0 ]; then
             echo "Error setting permissions for: $RUNTIME_DIR"
             exit 1
         fi
-        sudo chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR"
+        sudo chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR" 2>/dev/null
         if [ $? -ne 0 ]; then
             echo "Error setting ownership for: $RUNTIME_DIR"
             exit 1
         fi
     fi
 else
-    # Create the runtime directory
-    sudo mkdir -p "$RUNTIME_DIR"
+    # Create the runtime directory silently
+    sudo mkdir -p "$RUNTIME_DIR" 2>/dev/null
     if [ $? -ne 0 ]; then
         echo "Error creating runtime directory: $RUNTIME_DIR"
         exit 1
     fi
-    # Set ownership and permissions
-    sudo chmod 0700 "$RUNTIME_DIR"
+    # Set ownership and permissions silently
+    sudo chmod 0700 "$RUNTIME_DIR" 2>/dev/null
     if [ $? -ne 0 ]; then
         echo "Error setting permissions for: $RUNTIME_DIR"
         exit 1
     fi
-    sudo chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR"
+    sudo chown "$USER_ID:$GROUP_ID" "$RUNTIME_DIR" 2>/dev/null
     if [ $? -ne 0 ]; then
         echo "Error setting ownership for: $RUNTIME_DIR"
         exit 1
