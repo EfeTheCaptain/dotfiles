6281733a949d172cf18894aba6baf5d1ae53f809
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 01:58:38 +0300

    Update colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# CPU Model (Hardcoded, as per your preference)
+# CPU Model (Hardcoded, as preference)
 cpu_model="Intel-Atom-N550"
-
-# Package Count (using 'dpkg' for Debian/Ubuntu-based systems)
+# Package Count Debian/Ubuntu
 package_count=$(dpkg -l | grep '^ii' | wc -l)
+os_logo="󰣚"
 
 # Define the simplified prompt
 PS1="\n╭──[$cpu_model]─[$package_count] \n\
 ├[\u@\h]─[\w]\n\
-╰─[]──▶  "
+╰─[$os_logo]──▶  "
