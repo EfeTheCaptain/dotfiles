5782d313addf5d9a3fdccf400b011c3f81e47075
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 01:39:04 +0300

    Update colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -13,23 +13,24 @@ if [ ! -f "$CACHE_FILE" ]; then
     # Read the third word from board_name (board name)
     cache_reading_3=$(awk '{print $3}' /sys/class/dmi/id/board_name)
     
+    # Concatenate vendor and board name correctly (e.g., SAMSUNG-NF210)
+    cache_reading_2_board="${cache_reading_2}-${cache_reading_3}"
+
     # Read and format the CPU model (e.g., Intel(R) Atom(TM) CPU 550 @1.50GHz -> Intel-Atom-N550)
     cache_reading_1=$(awk -F: '/model name/ {print $2}' /proc/cpuinfo | head -n 1 | awk '{print $1"-"$2"-"$3}' | sed 's/Intel/Intel-/; s/Atom/Atom-/; s/([^)]+)//g')
 
     # Save the data to the cache file
-    echo "$cache_reading_2" > "$CACHE_FILE"
-    echo "$cache_reading_3" >> "$CACHE_FILE"
+    echo "$cache_reading_2_board" > "$CACHE_FILE"
     echo "$cache_reading_1" >> "$CACHE_FILE"
 else
     # Read from cache file
     mapfile -t cache_data < "$CACHE_FILE"
-    cache_reading_2="${cache_data[0]}"
-    cache_reading_3="${cache_data[1]}"
-    cache_reading_1="${cache_data[2]}"
+    cache_reading_2_board="${cache_data[0]}"
+    cache_reading_1="${cache_data[1]}"
 fi
 
 # Define the prompt without colors
-PS1="\n╭ ──[$cache_reading_2-$cache_reading_3]───╮\n\
+PS1="\n╭ ──[$cache_reading_2_board]───╮\n\
 ├──[$cache_reading_1]──────╯\n\
 ├[\u@\h]─[\w]\n\
 ╰──▶  "
