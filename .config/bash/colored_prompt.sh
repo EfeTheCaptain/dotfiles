6455f38a51e175e7120de9c5c42fa8a5c4c4b387
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 01:48:45 +0300

    Update colored_prompt.sh
    ?

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fetch and format the CPU model correctly
-cpu_model=$(grep -m 1 'model name' /proc/cpuinfo | sed -E 's/.*Intel.*Atom.*CPU ([0-9]+).*@([0-9.]+)GHz.*/Intel-Atom-\1/')
+cpu_model=$(grep -m 1 'model name' /proc/cpuinfo | sed -E 's/Intel\(R\) Atom\(TM\) CPU ([0-9]+) @.*Ghz.*/Intel-Atom-\1/')
 
 # Define the simplified prompt
 PS1="\n╭──[$cpu_model] \n\
