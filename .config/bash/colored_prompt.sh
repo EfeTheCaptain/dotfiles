0092e2606c3bf926ba0715c55a06cbbe8590c433
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 01:27:52 +0300

    Update colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -7,10 +7,10 @@ CACHE_FILE="$HOME/.cache/sys_info_cache"
 if [ ! -f "$CACHE_FILE" ]; then
     # Fetch and cache the system information
 
-    # Read the first word from sys_vendor
+    # Read the first word from sys_vendor (vendor)
     cache_reading_2=$(awk '{print $1}' /sys/class/dmi/id/sys_vendor)
     
-    # Read the third word from board_name
+    # Read the third word from board_name (board name)
     cache_reading_3=$(awk '{print $3}' /sys/class/dmi/id/board_name)
     
     # Read and format the CPU model (e.g., Intel(R) Atom(TM) CPU 550 @1.50GHz -> Intel-Atom-N550)
@@ -42,7 +42,10 @@ forest_shadow='\[\e[38;5;16m\]' # Dark Grey (Shadows)
 nc='\[\e[0m\]'
 
 # Define the prompt using cached data with color codes
-PS1="\n\[\e[1;${bark_brown}m\]╭ ──[\[$evergreen_dark\]$cache_reading_2-\[$evergreen_dark\]$cache_reading_3\]───╮\n\[\e[1;${bark_brown}m\]├──[\[$mossy_green\]$cache_reading_1\]──────╯\n\[\e[1;${bark_brown}m\]├[\[$sun_yellow\]\u\[$bright_sky\]@\[$forest_shadow\]\h\[$bright_sky\]]─[\w]\n\[\e[1;${bark_brown}m\]╰──▶  \[\e[0m\] "
+PS1="\n\[\e[1;${bark_brown}m\]╭ ──[\[$evergreen_dark\]$cache_reading_2-\[$evergreen_dark\]$cache_reading_3\]───╮\n\
+\[\e[1;${bark_brown}m\]├──[\[$mossy_green\]$cache_reading_1\]──────╯\n\
+\[\e[1;${bark_brown}m\]├[\[$sun_yellow\]\u\[$bright_sky\]@\[$forest_shadow\]\h\[$bright_sky\]]─[\[$mid_sky\]\w]\n\
+\[\e[1;${bark_brown}m\]├──▶  \[\e[0m\]"
 
 # Force color interpretation
 PS1="$(eval echo -e \"$PS1\")"
