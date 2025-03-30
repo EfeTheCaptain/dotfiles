492c6f03b7bfea23ac54c594e1cea9eb9cde2c1b
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 03:25:54 +0300

    Update colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -3,7 +3,7 @@
 # Color Variables
 color_083='\[\e[38;5;083m\]' # Bright Greenish Cyan
 color_005='\[\e[38;5;005m\]' # Dark Purple
-color_087='\[\e[38;5;087m\]' # Light Cyan
+color_024='\[\e[38;5;024m\]' # Light Cyan
 color_231='\[\e[38;5;231m\]' # White
 color_184='\[\e[38;5;184m\]' # Yellow-Green
 color_076='\[\e[38;5;076m\]' # Light Green
@@ -11,7 +11,7 @@ color_045='\[\e[38;5;045m\]' # Aqua Blue
 nc='\[\e[0m\]' # Reset
 
 # CPU Model (Hardcoded)
-cpu_model="${color_087}Intel-Atom-N550${nc}"
+cpu_model="${color_024}Intel-Atom-N550${nc}"
 
 # Package Count
 package_count="${color_005}$(dpkg -l | grep '^ii' | wc -l)${nc}"
@@ -19,8 +19,7 @@ package_count="${color_005}$(dpkg -l | grep '^ii' | wc -l)${nc}"
 # OS Logo (Debian logo unicode character)
 os_logo="${color_231}󰣇${nc}"
 
-# Define the Prompt
-PS1="\n${color_083}┏━${color_083}[${package_count}]${color_083}━━[${os_logo}${color_083}]\n\
-${color_083}┣━━${color_083}[${cpu_model}${color_083}]\n\
-${color_083}┣━━━[${color_184}\u${color_076}@\h${color_083}]━━━[${color_045}\w${color_083}]\n\
-${color_083}┗━🠊 ${nc} "
+echo -e "\n${color_084}┏━${color_084}[${package_count}${color_084}]━━[${os_logo}${color_084}]\n\
+echo -e "${color_084}┣━━${color_084}[${cpu_model}${color_084}]\n\
+PS1="${color_084}┣━━━[${color_184}\u${color_114}@\h${color_084}]━━━[${color_045}\w${color_084}]\n\
+${color_084}┗━🠊 ${nc} "
