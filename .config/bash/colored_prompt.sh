82eee8b7ce80a9a8824d1e6c05877c2d967d30b7
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 05:01:29 +0300

    Update colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -3,14 +3,15 @@
 # Define color variables
 COL_GREEN="\033[38;5;34m"
 COL_BLUE="\033[38;5;125m"
-COL_CYAN="\033[38;5;89m"
+COL_CYAN="\033[38;5;124m"
 COL_YELLOW="\033[38;5;220m"
 COL_RED="\033[38;5;45m"
+COL_WHITE="\[\033[38;5;194m\]"
 
 # Dynamic content generation (with PS1 escapes)
-package_count="${COL_BLUE}$(dpkg -l | grep -c '^ii')"
-cpu_model="${COL_CYAN}Intel-Atom-N550"
-os_logo="\U238" 
+package_count="${COL_CYAN}$(dpkg -l | grep -c '^ii')"
+cpu_model="${COL_BLUE}Intel-Atom-N550"
+os_logo="${COL_WHITE}" 
 
 # Static header display (using printf for reliability)
 printf "${COL_GREEN}╭──[ %b ${COL_GREEN}]───[ %b ${COL_GREEN}]\n" "$package_count" "$os_logo"
@@ -18,5 +19,5 @@ printf "${COL_GREEN}╰───[ %b ${COL_GREEN}]" "$cpu_model"
 
 # Interactive prompt (with proper \[ \] escaping)
 PS1="\n\
-\[\033[38;5;34m\]╭───[\[\033[38;5;220m\]\u\[\033[38;5;24m\]@\h\[\033[38;5;34m\]]──[\[\033[38;5;45m\]\w\[\033[38;5;34m\]]\n\
-\[\033[38;5;34m\]╰─🠞 "
+\[\033[38;5;34m\]╭───[ \[\033[38;5;220m\]\u\[\033[38;5;24m\]@\h\[\033[38;5;34m\] ]──[ \[\033[38;5;45m\]\w\[\033[38;5;34m\] ]\n\
+\[\033[38;5;34m\]╰─🠞 \[\033[38;5;224m\]"
