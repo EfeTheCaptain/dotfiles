0de7e7dd1e298bc4cdafc5c23f76c4a8fe612f3d
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 04:20:25 +0300

    Update colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -1,21 +1,13 @@
-# Color Variables (raw escape codes, no \[ \])
-color_193='\033[38;5;193m'
-color_005='\033[38;5;005m'
-color_087='\033[38;5;087m'
-color_231='\033[38;5;231m'
-color_184='\033[38;5;184m'
-color_114='\033[38;5;114m'
-color_045='\033[38;5;045m'
-nc='\033[0m'
-
 # Static Info
-cpu_model="$(nc)${color_140}Intel-Atom-N550${nc}"
-package_count="${color_087}$(dpkg -l | grep -c '^ii')${nc}"
-os_logo="${color_231}\Uf0238${nc}"  # Using Unicode escape for Debian logo
+package_count="\[\033[38;5;125m\]$(dpkg -l | grep -c '^ii')"
+cpu_model="\[\033[38;5;89m\]Intel-Atom-N550"
+os_logo="\Uf0238"  # Using Unicode escape for Debian logo
 
 # Print static header (correctly formatted)
-echo -e "\n${color_193}╭──[ ${package_count} ${color_193}]───[ ${os_logo} ${color_193}]"
-echo -e "${color_193}╰───[ ${cpu_model} ${color_193}]\n"
+echo -e "\n\[\033[38;5;220m\]╭──[ ${package_count} \[\033[38;5;220m\]]───[\[\033[0m\] ${os_logo} \[\033[38;5;220m\]]"
+echo -e "\[\033[38;5;220m\]╰───[ ${cpu_model} \[\033[38;5;220m\]]\n"
 
 # Define PS1 (with \[ \] for prompt safety)
-PS1="\n\[\033[38;5;193m\]╭───[\[\033[38;5;220m\]\u\[\033[38;5;157m\]@\h\[\033[38;5;193m\]]──[\[\033[38;5;045m\]\w\[\033[38;5;193m\]]\n\[\033[38;5;087m\]╰─🠞 \[\033[0m\] "
+PS1="\n\
+\[\033[38;5;220m\]╭───[\[\033[38;5;220m\]\u\\[\033[38;5;34m\]@\h\[\033[38;5;040m\]]──[\[\033[38;5;045m\]\w\[\033[38;5;040m\]]\n\
+\[\033[38;5;220m\]╰─🠞 \[\033[0m\] "
