18b040126f98502c01763788d3763abff4d5873d
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 03:03:59 +0300

    Update colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -20,7 +20,7 @@ package_count="${color_005}$(dpkg -l | grep '^ii' | wc -l)${nc}"
 os_logo="${color_231}󰣇${nc}"
 
 # Define the Prompt
-PS1="\n${color_083}┏━${color_083}[${package_count}]${color_083}━━[${os_logo}]${color_083}\n\
-${color_083}┣━━${color_083}[${cpu_model}]${color_083}\n\
-${color_083}┣━━━[${color_184}\u${color_076}\@h${color_083}]━━━[${color_045}\w]${color_083}\n\
+PS1="\n${color_083}┏━${color_083}[${package_count}]${color_083}━━[${os_logo}${color_083}]\n\
+${color_083}┣━━${color_083}[${cpu_model}${color_083}]\n\
+${color_083}┣━━━[${color_184}\u${color_076}\@h${color_083}]━━━[${color_045}\w${color_083}]\n\
 ${color_083}┗━🠊 ${nc} "
