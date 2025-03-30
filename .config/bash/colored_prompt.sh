c6bbbdf99e7c7523a82d39310ffc93c218d70e4a
EfeTheCaptain <65527605+EfeTheCaptain@users.noreply.github.com>
2025-03-30 03:40:11 +0300

    Update colored_prompt.sh

--- .config/bash/colored_prompt.sh
+++ .config/bash/colored_prompt.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-# Color Variables (updated with your colors)
-color_084='\e[38;5;084m'  # Note: Removed \[ \] for non-PS1 use
-color_005='\e[38;5;005m'
-color_087='\e[38;5;087m'
-color_231='\e[38;5;231m'
-color_184='\e[38;5;184m'
-color_114='\e[38;5;114m'
-color_045='\e[38;5;045m'
-nc='\e[0m'
+# Color Variables (raw escape codes, no \[ \])
+color_084='\033[38;5;084m'
+color_005='\033[38;5;005m'
+color_087='\033[38;5;087m'
+color_231='\033[38;5;231m'
+color_184='\033[38;5;184m'
+color_114='\033[38;5;114m'
+color_045='\033[38;5;045m'
+nc='\033[0m'
 
 # Static Info
 cpu_model="${color_087}Intel-Atom-N550${nc}"
-os_logo="${color_231}󰣇${nc}"
 package_count="${color_005}$(dpkg -l | grep -c '^ii')${nc}"
+os_logo="${color_231}\Uf0238${nc}"  # Using Unicode escape for Debian logo
 
-# Print static header (using printf for reliable Unicode)
-printf "\n${color_084}╭──[ %s ]───[ %s ]${nc}\n" "$package_count" "$os_logo"
-printf "${color_084}╰───[ %s ]${nc}\n\n" "$cpu_model"
+# Print static header (correctly formatted)
+echo -e "\n${color_084}╭──[ ${package_count} ]───[ ${os_logo} ]${nc}"
+echo -e "${color_084}╰───[ ${cpu_model} ]${nc}\n"
 
-# Define PS1 (restore \[ \] for prompt safety)
-color_084_ps1='\[\e[38;5;084m\]'  # PS1 needs \[ \] for cursor position
-color_184_ps1='\[\e[38;5;184m\]'
-color_114_ps1='\[\e[38;5;114m\]'
-color_045_ps1='\[\e[38;5;045m\]'
-nc_ps1='\[\e[0m\]'
-
-PS1="${color_084_ps1}╭───[${color_184_ps1}\u${color_114_ps1}@\h${color_084_ps1}]──[${color_045_ps1}\w${color_084_ps1}]\n${color_084_ps1}╰─🠞 ${nc_ps1} "
+# Define PS1 (with \[ \] for prompt safety)
+PS1="\n\[\033[38;5;084m\]╭───[\[\033[38;5;184m\]\u\[\033[38;5;114m\]@\h\[\033[38;5;084m\]]──[\[\033[38;5;045m\]\w\[\033[38;5;084m\]]\n\[\033[38;5;084m\]╰─🠞 \[\033[0m\] "
